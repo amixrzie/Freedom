a3a940185cbed9c76a963461f9600bbb1397da2f
amixrzi <amixrzie@gmail.com>
2025-08-20 00:36:28 +0330

    Convert project to dynamic WordPress version

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+get_header();
+
+if ( file_exists( get_template_directory() . '/front-page.php' ) ) {
+    include( get_template_directory() . '/front-page.php' );
+} else {
+    echo '<main class="max-w-7xl mx-auto px-6 mt-6"><p>هیچ محتوایی وجود ندارد.</p></main>';
+}
+
+get_footer();
